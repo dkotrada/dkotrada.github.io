959bd6b1acd4c1ca8da77d052466a832c42e6da4
Dieter Konrad <dk@off217.de>
2019-06-24 22:21:02 +0200

    pwa

---
+++ precache-manifest.599c0eb7bb2ecd2dcaf7010da0b5572c.js
@@ -0,0 +1,134 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8506bf6dcd5c8f5567eb",
+    "url": "css/app.71d1f814.css"
+  },
+  {
+    "revision": "5cb7edfceb233100075dc9a1e12e8da3",
+    "url": "fonts/KFOkCnqEu92Fr1MmgVxIIzQ.5cb7edfc.woff"
+  },
+  {
+    "revision": "87284894879f5b1c229cb49c8ff6decc",
+    "url": "fonts/KFOlCnqEu92Fr1MmEU9fBBc-.87284894.woff"
+  },
+  {
+    "revision": "b00849e00f4c2331cddd8ffb44a6720b",
+    "url": "fonts/KFOlCnqEu92Fr1MmSU5fBBc-.b00849e0.woff"
+  },
+  {
+    "revision": "adcde98f1d584de52060ad7b16373da3",
+    "url": "fonts/KFOlCnqEu92Fr1MmWUlfBBc-.adcde98f.woff"
+  },
+  {
+    "revision": "bb1e4dc6333675d11ada2e857e7f95d7",
+    "url": "fonts/KFOlCnqEu92Fr1MmYUtfBBc-.bb1e4dc6.woff"
+  },
+  {
+    "revision": "60fa3c0614b8fb2f394fa29944c21540",
+    "url": "fonts/KFOmCnqEu92Fr1Mu4mxM.60fa3c06.woff"
+  },
+  {
+    "revision": "29b882f018fa6fe75fd338aaae6235b8",
+    "url": "fonts/flUhRq6tzZclQEJ-Vdg-IuiaDsNa.29b882f0.woff"
+  },
+  {
+    "revision": "0509ab09c1b0d2200a4135803c91d6ce",
+    "url": "fonts/flUhRq6tzZclQEJ-Vdg-IuiaDsNcIhQ8tQ.0509ab09.woff2"
+  },
+  {
+    "revision": "c3e886514c633b21c81db623874d06cd",
+    "url": "img/quasar-logo-full.c3e88651.svg"
+  },
+  {
+    "revision": "9eaf20edeeee581d2c7e88d223047d37",
+    "url": "index.html"
+  },
+  {
+    "revision": "081658aacd70aa211b0f",
+    "url": "js/2d22c0ff.693506dc.js"
+  },
+  {
+    "revision": "c5fe31331376ffaa7d92",
+    "url": "js/461a5054.da826b56.js"
+  },
+  {
+    "revision": "76ef6467900d3810eb7c",
+    "url": "js/4b47640d.0143f3ac.js"
+  },
+  {
+    "revision": "8506bf6dcd5c8f5567eb",
+    "url": "js/app.f4098c62.js"
+  },
+  {
+    "revision": "d8a7b6d6d81c55a6750a",
+    "url": "js/vendor.c112ca72.js"
+  },
+  {
+    "revision": "3bad37a1c391acb291957eb183ec581b",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "bb6096a5adbe424829100eab93ce94cd",
+    "url": "statics/app-logo-128x128.png"
+  },
+  {
+    "revision": "51332019b3dd4a837cdab192027d7e79",
+    "url": "statics/icons/apple-icon-120x120.png"
+  },
+  {
+    "revision": "672f3176311f246aecdd2e5ae776ce16",
+    "url": "statics/icons/apple-icon-152x152.png"
+  },
+  {
+    "revision": "b37720a9b45cab1595b16943ef90c302",
+    "url": "statics/icons/apple-icon-167x167.png"
+  },
+  {
+    "revision": "b4ed2d94bbf36b5ac1503baec659b7eb",
+    "url": "statics/icons/apple-icon-180x180.png"
+  },
+  {
+    "revision": "49b4980b59019a95c2020dd688f4bc78",
+    "url": "statics/icons/favicon-16x16.png"
+  },
+  {
+    "revision": "59d6646da622eb7d0c5af2aabbe358d4",
+    "url": "statics/icons/favicon-32x32.png"
+  },
+  {
+    "revision": "09a51c0633277dd0a2475f0e33daa901",
+    "url": "statics/icons/favicon-96x96.png"
+  },
+  {
+    "revision": "8273ebbd88ce37b1b4b57c3d730736b3",
+    "url": "statics/icons/favicon.ico"
+  },
+  {
+    "revision": "bb6096a5adbe424829100eab93ce94cd",
+    "url": "statics/icons/icon-128x128.png"
+  },
+  {
+    "revision": "79cef9e199fb56a72566495a4d554de1",
+    "url": "statics/icons/icon-192x192.png"
+  },
+  {
+    "revision": "6fef0211e9330ba3ab8398a407ff3fed",
+    "url": "statics/icons/icon-256x256.png"
+  },
+  {
+    "revision": "3d63ad6f72969746ba348212646740b8",
+    "url": "statics/icons/icon-384x384.png"
+  },
+  {
+    "revision": "6262bf439fa9caac88ca4afeea774b4e",
+    "url": "statics/icons/icon-512x512.png"
+  },
+  {
+    "revision": "9d6d5e55a4fca87f53f96cb06ad631f7",
+    "url": "statics/icons/ms-icon-144x144.png"
+  },
+  {
+    "revision": "8804de6f75ccfead5e138ddd9573c136",
+    "url": "statics/icons/safari-pinned-tab.svg"
+  }
+]);
